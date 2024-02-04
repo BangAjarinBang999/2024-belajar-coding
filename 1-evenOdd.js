07099d97ed307def38aad896041d3c73d2558f87
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-04 14:06:06 +0700

    1/366

--- 1-evenOdd.js
+++ 1-evenOdd.js
@@ -1,11 +1,14 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+// 1/366
+
 function evenOrOdd(number) {
-  //   return (result = number % 2 == 0 ? "Even" : "Odd");
-  const num = number;
-  if (num % 2 == 0) {
-    console.log("Even");
-  } else {
-    console.log("Odd");
-  }
+  return (result = number % 2 == 0 ? "Even" : "Odd");
+  //   const num = number;
+  //   if (num % 2 == 0) {
+  //     console.log("Even");
+  //   } else {
+  //     console.log("Even");
+  //   }
 }
 
 console.log(evenOrOdd(2));

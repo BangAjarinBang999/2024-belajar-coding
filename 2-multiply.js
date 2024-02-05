08d596dde94c0602dda4ffd39a278d7b97b33fa6
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-05 20:14:41 +0700

    2/366

---
+++ 2-multiply.js
@@ -0,0 +1,24 @@
+// 2/366
+//https://www.codewars.com/kata/50654ddff44f800200000004
+
+//dart
+// int multiply(int a, int b) {
+//   return a * b;
+// }
+
+// void main() {
+//   multiply(1, 1);
+// }
+// js
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(1, 1));
+console.log(multiply(2, 1));
+console.log(multiply(2, 2));
+console.log(multiply(3, 5));
+console.log(multiply(5, 0));
+console.log(multiply(0, 5));
+console.log(multiply(0, 0));

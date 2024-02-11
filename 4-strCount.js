bc9df6b64e90026f7d7b17b8f82e298da329817e
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-11 20:53:46 +0700

    4/366

--- 4-strCount.js
+++ 4-strCount.js
@@ -1,4 +1,14 @@
+// 4/366
+// https://www.codewars.com/kata/5865918c6b569962950002a1
+
 function strCount(str, letter) {
-  return str.split(letter).length - 1;
+  var result = 0;
+  for (let index = 0; index < str.length; index++) {
+    if (str[index] == letter) {
+      result += 1;
+    }
+  }
+  return result;
+  // return str.split(letter).length - 1;
 }
 console.log(strCount("", "l"));

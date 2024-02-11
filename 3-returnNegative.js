34a3e4d6c2b083492263be39bf0dc761b7e61f82
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-11 20:42:16 +0700

    3/366

--- 3-returnNegative.js
+++ 3-returnNegative.js
@@ -1,9 +1,12 @@
+// 3/366
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
 function makeNegative(num) {
-  return (result = num <= 0 ? num : -Math.abs(num));
+  //   return (result = num <= 0 ? num : -Math.abs(num));
+  return num <= 0 ? num : -num;
   //   if (num <= 0) {
   //     return num;
   //   } else {
   //     return -Math.abs(num)
   //   }
 }
-console.log(makeNegative(3));
+console.log(makeNegative(0));

73fea02999dba0919d188950ec6fd17201c4398e
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-15 18:55:43 +0700

    6/366

--- 6-sumOfPositive.js
+++ 6-sumOfPositive.js
@@ -1,19 +1,19 @@
 // https://www.codewars.com/kata/5715eaedb436cf5606000381
 // 6/366
-
-function positiveSum(arr) {
-  var sum = 0;
-  for (const index in arr) {
-    if (arr[index] > 0) {
-      sum += arr[index];
-    }
-  }
-  return sum;
-  //   for (let index = 0; index < arr.length; index++) {
-  //     if (arr[index] > 0) {
-  //       //   sum = sum + arr[index];
-  //       sum += arr[index];
-  //     }
-  //   }
-}
+const positiveSum = (arr) => arr.reduce((sum, n) => (n > 0 ? sum + n : sum), 0);
+// function positiveSum(arr) {
+//   var sum = 0;
+//   for (const index in arr) {
+//     if (arr[index] > 0) {
+//       sum += arr[index];
+//     }
+//   }
+//   return sum;
+//   //   for (let index = 0; index < arr.length; index++) {
+//   //     if (arr[index] > 0) {
+//   //       //   sum = sum + arr[index];
+//   //       sum += arr[index];
+//   //     }
+//   //   }
+// }
 console.log(positiveSum([-1, -2, 3, 4, -5]));

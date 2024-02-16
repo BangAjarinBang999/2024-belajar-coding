664dc872f1182ad6e6d461924060e4c4d1fa1e11
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-16 21:13:13 +0700

    8/366

---
+++ 8-convertBoolValuesToStrings.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/
+// 8/366
+
+const boolToWord = (bool) => (bool ? "Yes" : "No");
+// function boolToWord(bool) {
+//   if ((bool == true)) {
+//     return "Yes";
+//   } else if ((bool == false)) {
+//     return "No";
+//   }
+//   return bool;
+// }
+
+console.log(boolToWord(false));

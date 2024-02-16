d057850c4e595debbbf1dc501e62e872df101244
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-16 21:23:04 +0700

    9/366

---
+++ 9-convertNumberToString.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+// 9/366
+
+// const numberToString = (num) => (num.toString());
+function numberToString(num) {
+  return num.toString();
+}
+
+console.log(numberToString(21431));

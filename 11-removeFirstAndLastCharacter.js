93bb50edb7c12482cf3493af7ecaa9a2bea6c07b
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-18 21:08:12 +0700

    11/366

--- 11-removeFirstAndLastCharacter.js
+++ 11-removeFirstAndLastCharacter.js
@@ -1,7 +1,8 @@
 // https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
 // 11/366
 
-const removeChar = (str) => str.substring(1, str.length - 1);
+// const removeChar = (str) => str.substring(1, str.length - 1);
+const removeChar = (str) => str.slice(1, -1);
 
 // function removeChar(str) {
 //     return str.substring(1, str.length - 1);

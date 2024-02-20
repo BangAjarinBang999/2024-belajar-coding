db4a9409f44fbdcc67584b9794748ece8e6f06b3
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-20 13:27:00 +0700

    13/366

---
+++ 13-stringRepeat.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+// 13/366
+
+const repeatStr = (n, s) => s.repeat(n);
+
+// function repeatStr(n, s) {
+//   var result = "";
+//   for (let index = 0; index < n; index++) {
+//     result += s;
+//   }
+//   return result;
+// }
+
+console.log(repeatStr(3, "he "));

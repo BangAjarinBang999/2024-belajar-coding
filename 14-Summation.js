c0e82635d5630451ded7c711ffeb49429719edb0
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-22 20:11:40 +0700

    14/366

---
+++ 14-Summation.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+// 14/366
+
+const summation = (num) => (num * (num + 1)) / 2;
+// function summation(num) {
+//   var sum = 0;
+//   for (let index = 1; index <= num; index++) {
+//     sum += index;
+//   }
+//   return sum;
+// }
+console.log(summation(2));

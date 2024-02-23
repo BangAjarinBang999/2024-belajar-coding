3fff97ee852e240b6893a2c205d3c68bb9b50fbd
fajar <129683654+BangAjarinBang999@users.noreply.github.com>
2024-02-23 15:30:56 +0700

    19/366

---
+++ 19-stringToNumber.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
+// 19/366
+
+const stringToNumber = function (str) {
+  return +str; // parseInt(str);
+};
+console.log(stringToNumber("123"));
